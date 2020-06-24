043163beab0c2373ae34049f2359524c394d4309
bhanu <bhanusbc@hotmail.com>
2020-06-24 12:11:41 +0530

    added ProgamDemo1

---
+++ ProgramDemo1.java
@@ -0,0 +1,8 @@
+public class ProgramDemo1
+{
+  public static void main(String[] args)
+  {
+     System.out.println("Welcome to Hello World");
+     System.out.println("Hi This is ProgramDemo1");
+  }
+}
